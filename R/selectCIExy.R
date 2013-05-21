01f8179955d45602ef34f0e0c43b8888ec345632
Bryan Hanson <hanson@depauw.edu>
2013-05-21 08:04:38 -0400

    selectCIExy & selectCalCols now find bands correctly

--- R/selectCIExy.R
+++ R/selectCIExy.R
@@ -6,8 +6,16 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 	# Part of the photoSpec package
 	# Major contributions from Matthew Kukurugya
 
+	diagnostics <- TRUE # maybe make this an argument
+	
 ##### Helper Functions #####
-# See gist.github.com/bryanhanson/5471173
+
+	dAB <- function(A, B) { # distance between pts A & B
+		# A, B each as c(x, y)
+		dAB <- sqrt((B[2]-A[2])^2 + (B[1]-A[1])^2)
+		}
+		
+# gist.github.com/bryanhanson/5471173 has more info on these next ones
 
 	getBoundingBox <- function(P0, P1) {
 	
@@ -96,27 +104,29 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 
 ##### End of Helper Functions #####
 
+	# Check input data
 	if (is.null(L1)) stop("You must give a wavelength for L1")
 	if (is.null(L2)) stop("You must give a wavelength for L2")
 	if (L1 > L2) stop("L1 must be less than L2")
+
 	# Re-order things so that L1 < L2 < L3 in what comes later
 	L3 <- L2	
 	L2 <- L1 + 0.5*(L2-L1) # This will be the mid point
 	
-	# load data (shark fin)
+	# Load spectral locus data (shark fin)
 	data(CIExyz)
 	xy <- CIExyz[,c(2,3)] # 4400 rows
 	xy <- rbind(xy, xy[1,]) # repeat row so that polygon can close
 
-	# Find the coordinates of the given wavelengths
+	# Find the coordinates of the input wavelengths
 	ans1 <- grep(L1, CIExyz$wavelength)
 	ans1 <-ans1[1] # this is an index/row number
 	ans2 <- grep(L2, CIExyz$wavelength)
 	ans2 <-ans2[1]
 	ans3 <- grep(L3, CIExyz$wavelength)
 	ans3 <-ans3[1]
-
 	D65 <- getWhiteValues("D65")
+
 	# Put all the needed data in one place: p1 from L1, p2 from L2, p3 from L3
 	segs <- data.frame(
 		x = c(xy[ans1,1], xy[ans2,1], xy[ans3,1], D65[1,1]),
@@ -124,10 +134,11 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 	row.names(segs) <- c("p1", "p2", "p3", "D65")
 	in.segs <- segs # save a copy for later
 	
-	###### Flip the line segments 180 degrees and extend to ensure intersection
-		
+	# Flip the line segments 180 degrees and extend to ensure intersection
+	# FIX: some of these terms are zero and can be dropped
+	
 	ang <- pi
-	fac <- 2.0 # this ensures that the line segment is long enough to reach
+	fac <- 2.0 # Ensures that the line segment is long enough to reach
 	# the far side of the shark fin or line of purples
 	segs[1,1] = segs[4,1] + (cos(ang) * (segs[1,1] - segs[4,1]) + sin(ang) * (segs[1,2] - segs[4,2]))*fac
 	segs[1,2] = segs[4,2] + (-sin(ang) * (segs[1,2] - segs[4,2]) + cos(ang) * (segs[1,2] - segs[4,2]))*fac
@@ -138,13 +149,12 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 	segs[3,1] = segs[4,1] + (cos(ang) * (segs[3,1] - segs[4,1]) + sin(ang) * (segs[3,2] - segs[4,2]))*fac
 	segs[3,2] = segs[4,2] + (-sin(ang) * (segs[3,2] - segs[4,2]) + cos(ang) * (segs[3,2] - segs[4,2]))*fac
 	row.names(segs) <- c("p4", "p5", "p6", "D65")
-	# print(segs)
 	
 	# Now loop over shark fin and check for intersections	
 
 	keep <- c() # keep will always be length 3
 
-	for (i in 1:3) { # loop over the 3 line segments
+	for (i in 1:3) { # Loop over the 3 line segments
 		its <- nrow(xy)-1
 	
 		# Check to see if the line segments intersect
@@ -158,7 +168,7 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
  		} # end of loop that checks each line segment
 
 	# Here are the cases to be considered next.
-	# Segments are in wavelenght order.
+	# Segments are in wavelength order.
 	# 1.  no line segment intersects the line of purples.
 	# 2.  segment 1 hits the l of p, segments 2 & 3 the shark fin
 	# 3.  segments 1 & 2 hit the l of p, segment 3 the shark fin
@@ -168,10 +178,15 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 	# 7.  segments 1 & 2 hit shark fin, segment 3 the l of p
 	# The line of purples is segment xy[c(4400,4401),]
 
-	# We are also going to store the intersection points p4, p5 & p6:
+	# Store the intersection points p4, p5 & p6:
 	# L1/p1 -> D65 -> p4; L2/p2 -> D65 -> p5; L3/p3 -> D65 -> p6
+	# Record the 'case' and the point in the wedge farthest
+	# from D65 (xPt)
 	
 	Case1 <- Case2 <- Case3 <- Case4 <- Case5 <- Case6 <- Case7 <- FALSE
+	Case <- NA
+	xPt <- vector("numeric", 2)
+	
 	if (!4400 %in% keep) Case1 <- TRUE
 	if ((keep[1] == 4400) && (!keep[2] == 4400) && (!keep[3] == 4400)) Case2 <- TRUE 	
 	if ((keep[1] == 4400) && (keep[2] == 4400) && (!keep[3] == 4400)) Case3 <- TRUE 	
@@ -181,14 +196,26 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 	if ((!keep[1] == 4400) && (!keep[2] == 4400) && (keep[3] == 4400)) Case7 <- TRUE 	
 	
 	if (Case1) { # Case 1 (OK)
+		Case <- "Case1"
 	 	verts <- rbind(CIExyz[keep[1]:keep[3],], CIExyz[keep[3]+1,], c(NA, segs[4,1], segs[4,2], NA))
 	 	verts <- verts[,2:3]
 	 	p4 <- CIExyz[keep[1],2:3]
 	 	p5 <- CIExyz[keep[2],2:3]
-	 	p6 <- CIExyz[keep[3],2:3]	 	
+	 	p6 <- CIExyz[keep[3],2:3]
+	 	# Figure xPt
+	 	d4 <- dAB(D65, p4)
+	 	d5 <- dAB(D65, p5)
+	 	d6 <- dAB(D65, p6)
+	 	l <- c(d4, d5, d6)
+	 	xPt <- which.max(l) # FIX: something is up here
+	 	if (length(xPt) > 1) xPt <- xPt[1]
+	 	if (xPt == 1) xPt <- p4
+	 	if (xPt == 2) xPt <- p5
+	 	if (xPt == 3) xPt <- p6
  	 	}
 		
 	if (Case2) { # Case 2 (OK)
+		Case <- "Case2"
 		p4 <- lineIntersection(in.segs[1,1], in.segs[1,2], in.segs[4,1],
 			in.segs[4,2], xy[4400,1], xy[4400,2], xy[4401,1], xy[4401,2])
 	 	p5 <- CIExyz[keep[2],2:3]
@@ -196,9 +223,22 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 	 	verts <- rbind(c(NA, segs[4,1], segs[4,2], NA),
 	 		c(NA, p4[1], p4[2], NA), CIExyz[1:keep[3],])
 	 	verts <- verts[,2:3]
+	 	# Figure xPt
+	 	d4 <- dAB(D65, p4)
+	 	d5 <- dAB(D65, p5)
+	 	d6 <- dAB(D65, p6)
+	 	xy1 <- dAB(D65, CIExyz[1,2:3])
+	 	l <- c(d4, d5, d6, xy1)
+	 	xPt <- which.max(l)
+	 	if (length(xPt) > 1) xPt <- xPt[1]
+	 	if (xPt == 1) xPt <- p4
+	 	if (xPt == 2) xPt <- p5
+	 	if (xPt == 3) xPt <- p6
+	 	if (xPt == 4) xPt <- CIExyz[1,2:3]
 		}
 		
 	if (Case3) { # Case 3 (OK)
+		Case <- "Case3"
 		p4 <- lineIntersection(in.segs[1,1], in.segs[1,2], in.segs[4,1],
 			in.segs[4,2], xy[4400,1], xy[4400,2], xy[4401,1], xy[4401,2])
 		p5 <- lineIntersection(in.segs[2,1], in.segs[2,2], in.segs[4,1],
@@ -207,9 +247,21 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 	 	verts <- rbind(c(NA, segs[4,1], segs[4,2], NA),
 	 		c(NA, p4[1], p4[2], NA), CIExyz[1:keep[3],])
 	 	verts <- verts[,2:3]
+	 	# Figure xPt
+	 	d4 <- dAB(D65, p4)
+	 	d5 <- dAB(D65, p5)
+	 	d6 <- dAB(D65, p6)
+	 	xy1 <- dAB(D65, CIExyz[1,2:3])
+	 	l <- c(d4, d5, d6, xy1)
+	 	xPt <- which.max(l)
+	 	if (length(xPt) > 1) xPt <- xPt[1]
+	 	if (xPt == 2) xPt <- p5
+	 	if (xPt == 3) xPt <- p6
+	 	if (xPt == 4) xPt <- CIExyz[1,2:3]
 		}
 
 	if (Case4) { # Case 4 (OK)
+		Case <- "Case4"
 		p4 <- lineIntersection(in.segs[1,1], in.segs[1,2], in.segs[4,1],
 			in.segs[4,2], xy[4400,1], xy[4400,2], xy[4401,1], xy[4401,2])
 		p5 <- lineIntersection(in.segs[2,1], in.segs[2,2], in.segs[4,1],
@@ -218,18 +270,47 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 			in.segs[4,2], xy[4400,1], xy[4400,2], xy[4401,1], xy[4401,2])
 		verts <- rbind(segs[4,], p4, p6)
 		colnames(verts) <- c("x", "y")
+	 	# Figure xPt
+	 	d4 <- dAB(D65, p4)
+	 	d5 <- dAB(D65, p5)
+	 	d6 <- dAB(D65, p6)
+	 	l <- c(d4, d5, d6)
+	 	xPt <- which.max(l) # FIX: something is up here
+	 	if (length(xPt) > 1) xPt <- xPt[1]
+	 	str(xPt)
+	 	if (xPt == 1) xPt <- p4
+	 	if (xPt == 2) xPt <- p5
+	 	if (xPt == 3) xPt <- p6
 		}
 
 	if (Case5) { # Case 5 (OK)
+		Case <- "Case5"
 	 	p4 <- CIExyz[keep[1],2:3]
 		p5 <- lineIntersection(in.segs[2,1], in.segs[2,2], in.segs[4,1],
 			in.segs[4,2], xy[4400,1], xy[4400,2], xy[4401,1], xy[4401,2])
 	 	p6 <- CIExyz[keep[3],2:3]
 	 	verts <- rbind(CIExyz[keep[3]:4400,], CIExyz[1:keep[1],], c(NA, segs[4,1], segs[4,2], NA))
 	 	verts <- verts[,2:3]
- 	 	}
+	 	# Figure xPt
+	 	d4 <- dAB(D65, p4)
+	 	d5 <- dAB(D65, p5)
+	 	d6 <- dAB(D65, p6)
+	 	xy1 <- as.numeric(CIExyz[1,2:3])
+	 	xy4400 <- as.numeric(CIExyz[4400,2:3])
+	 	dxy1 <- dAB(D65, xy1)
+	 	dxy4400 <- dAB(D65, xy4400)
+	 	l <- c(d4, d5, d6, dxy1, dxy4400)
+	 	xPt <- which.max(l)
+	 	if (length(xPt) > 1) xPt <- xPt[1]
+	 	if (xPt == 1) xPt <- p4
+	 	if (xPt == 2) xPt <- p5
+	 	if (xPt == 3) xPt <- p6
+	 	if (xPt == 4) xPt <- xy1
+	 	if (xPt == 5) xPt <- xy4400
+	 	}
 
 	if (Case6) { # Case 6 (OK)
+		Case <- "Case6"
 	 	p4 <- CIExyz[keep[1],2:3]
 		p5 <- lineIntersection(in.segs[2,1], in.segs[2,2], in.segs[4,1],
 			in.segs[4,2], xy[4400,1], xy[4400,2], xy[4401,1], xy[4401,2])
@@ -238,9 +319,26 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 	 	verts <- rbind(CIExyz[keep[1]:4400,], c(NA, p5[1], p5[2], NA),
 	 		c(NA, p6[1], p6[2], NA), c(NA, segs[4,1], segs[4,2], NA))
 	 	verts <- verts[,2:3]
+	 	# Figure xPt
+	 	d4 <- dAB(D65, p4)
+	 	d5 <- dAB(D65, p5)
+	 	d6 <- dAB(D65, p6)
+	 	xy1 <- as.numeric(CIExyz[1,2:3])
+	 	xy4400 <- as.numeric(CIExyz[4400,2:3])
+	 	dxy1 <- dAB(D65, xy1)
+	 	dxy4400 <- dAB(D65, xy4400)
+	 	l <- c(d4, d5, d6, dxy1, dxy4400)
+	 	xPt <- which.max(l)
+	 	if (length(xPt) > 1) xPt <- xPt[1]
+	 	if (xPt == 1) xPt <- p4
+	 	if (xPt == 2) xPt <- p5
+	 	if (xPt == 3) xPt <- p6
+	 	if (xPt == 4) xPt <- xy1
+	 	if (xPt == 5) xPt <- xy4400
  	 	}
 
 	if (Case7) { # Case 7 (OK)
+		Case <- "Case7"
 	 	p4 <- CIExyz[keep[1],2:3]
 	 	p5 <- CIExyz[keep[2],2:3]
 		p6 <- lineIntersection(in.segs[3,1], in.segs[3,2], in.segs[4,1],
@@ -248,13 +346,26 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 	 	verts <- rbind(CIExyz[keep[1]:4400,],
 	 		c(NA, p6[1], p6[2], NA), c(NA, segs[4,1], segs[4,2], NA))
 	 	verts <- verts[,2:3]
- 	 	}
+	 	# Figure xPt
+	 	d4 <- dAB(D65, p4)
+	 	d5 <- dAB(D65, p5)
+	 	d6 <- dAB(D65, p6)
+	 	xy4400 <- as.numeric(CIExyz[4400,2:3])
+	 	dxy4400 <- dAB(D65, xy4400)
+	 	l <- c(d4, d5, d6, dxy4400)
+	 	xPt <- which.max(l)
+	 	if (length(xPt) > 1) xPt <- xPt[1]
+	 	if (xPt == 1) xPt <- p4
+	 	if (xPt == 2) xPt <- p5
+	 	if (xPt == 3) xPt <- p6
+	 	if (xPt == 4) xPt <- xy4400
+	 	}
 
  	# Now that the proper vertices have been selected, do the plot
 	bgr <- plotCIEchrom(gradient = verts, colSpace, ff, opts = c())
 	grid.polygon(verts$x, verts$y, default.units = "native")
 	grid.segments(x0 = in.segs[c(1,3),1], y0 = in.segs[c(1,3),2], # these are the dotted lines
-		x1 = in.segs[c(4,4),1], y1 = in.segs[c(4,4),2], # from L1 and L2
+		x1 = in.segs[c(4,4),1], y1 = in.segs[c(4,4),2], # from L1 and L2 to D65
 		default.units = "native", gp = gpar(lty = 2))
 
 	# Prepare the enclosed colors to serve as the calibration colors
@@ -265,27 +376,32 @@ selectCIExy <- function(L1 = NULL, L2 = NULL, colSpace = "sRGB", ff = 1.0, ...) 
 	bgr <- as.vector(bgr)
 	bgr <- bgr[bgr != "#FFFFFF"]
 
-	message("Total colors selected:", length(bgr))
+	message("Total colors to choose from:", length(bgr))
 	
 	# Assemble a list for return
 	wedge <- vector("list")
-	wedge$CIEcols <- bgr
-	wedge$verts <- verts
-	wedge$wavelength <- c(L1, L2)
-	wedge$p4 <- p4
-	wedge$p5 <- p5
-	wedge$p6 <- p6
+	wedge$wavelength <- c(L1, L2) # input wavelengths
+	wedge$Case <- Case
+	wedge$verts <- verts # vertices of wedge
+	wedge$CIEcols <- bgr # colors in wedge
 	wedge$colSpace <- colSpace
 	wedge$ff <- ff
+	wedge$p4 <- p4 # see above for defs of these pts
+	wedge$p5 <- p5
+	wedge$p6 <- p6
+	wedge$xPt <- xPt
 	
-	grid.points(x = p4[1], y = p4[2], default.units = "native",
-		gp = gpar(col = "red"), size = unit(0.5, "char"))
-	grid.points(x = p5[1], y = p5[2], default.units = "native",
-		gp = gpar(col = "red"), size = unit(0.5, "char"))
-	grid.points(x = p6[1], y = p6[2], default.units = "native",
-		gp = gpar(col = "red"), size = unit(0.5, "char"))
-	grid.points(x = segs[4,1], y =segs[4,2], default.units = "native",
-		gp = gpar(col = "red"), size = unit(0.5, "char"))
+	if (diagnostics) {
+		grid.points(x = p4[1], y = p4[2], default.units = "native",
+			gp = gpar(col = "red"), size = unit(0.5, "char"))
+		grid.points(x = p5[1], y = p5[2], default.units = "native",
+			gp = gpar(col = "red"), size = unit(0.5, "char"))
+		grid.points(x = p6[1], y = p6[2], default.units = "native",
+			gp = gpar(col = "red"), size = unit(0.5, "char"))
+		grid.points(x = segs[4,1], y =segs[4,2], default.units = "native",
+			gp = gpar(col = "red"), size = unit(0.5, "char"))
+		}
+
 	invisible(wedge)
 
 	} # end of function
--- R/selectCalCols.R
+++ R/selectCalCols.R
@@ -9,7 +9,7 @@ selectCalCols <- function(wedge, nDiv = 10, pcpd = 1, divMode = "linear") {
 	# reduce them in a sensible way to a limited no. of values for
 	# use as calibration colors
 	
-	# wedge is the data coming from selectCIExy
+	# wedge is the data coming from selectCIExy (see below)
 	# nDiv is the no. of divisions to create
 	# pcpd is the no. of paint chips per division
 	# total paint chips = nDiv * pcpd
@@ -17,7 +17,7 @@ selectCalCols <- function(wedge, nDiv = 10, pcpd = 1, divMode = "linear") {
 	
 	# consider adding a plot showing the results
 
-	### Helper function
+##### Helper functions
 
 	ang0to2pi <- function(segment) {
 		# segment given as c(x1, y1, x2, y2)
@@ -30,30 +30,38 @@ selectCalCols <- function(wedge, nDiv = 10, pcpd = 1, divMode = "linear") {
 		return(theta)
 		}
 
-	# Assemble a list for return
+	dAB <- function(A, B) { # distance between pts A & B
+		# A, B each as c(x, y)
+		dAB <- sqrt((B[2]-A[2])^2 + (B[1]-A[1])^2)
+		}
+
+##### End of helper functions
+
 	# wedge <- vector("list")
-	# wedge$CIEcols <- bgr
-	# wedge$verts <- verts
-	# wedge$wavelength <- c(L1, L2)
-	# wedge$p4 <- p4
-	# wedge$p5 <- p5
-	# wedge$p6 <- p6
+	# wedge$wavelength <- c(L1, L2) # input wavelengths
+	# wedge$Case <- Case
+	# wedge$verts <- verts # vertices of wedge
+	# wedge$CIEcols <- bgr # colors in wedge
 	# wedge$colSpace <- colSpace
 	# wedge$ff <- ff
+	# wedge$p4 <- p4 # see above for defs of these pts
+	# wedge$p5 <- p5
+	# wedge$p6 <- p6
+	# wedge$xPt <- xPt
 
-	# Prepare the starting data
+	### Prepare the starting data
 	D65 <- unlist(getWhiteValues("D65"))
 	p4 <- unlist(wedge$p4)
 	p5 <- unlist(wedge$p5)
 	p6 <- unlist(wedge$p6)
+	xPt <- unlist(wedge$xPt)
 	
 	### Divide the wedge up into nDiv bands using divMode
 	
 	if (divMode == "linear") { # divide the radius up into equal parts
 		
-		# First stab, use midpoint p5 (won't work in many cases)
-		# Get distance along radius for each band
-		div <- (sqrt((p5[1] - D65[1])^2 + (p5[2] - D65[2])^2))/nDiv
+		# Divide up the distance from D65 to xPt equally
+		div <- (sqrt((xPt[1] - D65[1])^2 + (xPt[2] - D65[2])^2))/nDiv
 		rad <- seq(0, nDiv*div, by = div)
 		rad <- rad[-1]
 		} # end of divMode linear
@@ -62,10 +70,14 @@ selectCalCols <- function(wedge, nDiv = 10, pcpd = 1, divMode = "linear") {
 
 	ang1 <- ang0to2pi(c(D65[1], D65[2], p4[1], p4[2]))
 	ang2 <- ang0to2pi(c(D65[1], D65[2], p6[1], p6[2]))
-	# cat("angle 1 =", ang1, "\n")
-	# cat("angle 2 =", ang2, "\n")
 	angs <- sort(c(ang1, ang2))
-	angs <- seq(angs[1], angs[2], by = 2*pi/360)
+	myby <- 2*pi/360	
+	if ((ang2-ang1) > pi) {
+		cat("weird\n")
+		angs[2] <- -1*(2*pi - angs[2])
+		myby <- -1*myby
+		}
+	angs <- seq(angs[1], angs[2], by = myby)
 	# angles along arc every 1 degree
 	# ang2 always > ang1
 
@@ -81,13 +93,11 @@ selectCalCols <- function(wedge, nDiv = 10, pcpd = 1, divMode = "linear") {
 		bands[[i]]$x <- c(bands[[i]]$x, rev(bands[[i-1]]$x))
 		bands[[i]]$y <- c(bands[[i]]$y, rev(bands[[i-1]]$y))
 		}
-	bands[[1]]$x <- c(bands[[1]]$x, D65[1]) # fix 1st one
+	bands[[1]]$x <- c(bands[[1]]$x, D65[1]) # fix 1st band
 	bands[[1]]$y <- c(bands[[1]]$y, D65[2])
 	
-#	str(bands)	
-
 	bands
 
-	# Next: Sample colors within each band
+	### Next: Sample colors within each band
 
 	} # end of function
